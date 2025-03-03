2ba1d6eb312248451cf5412e11918344892df85d
mundgus <fabnor99@gmail.com>
2025-03-03 21:51:56 +0100

    feat: initializr spring project

---
+++ src/main/java/hu/mundgus/udemxcarrent/UdemxcarrentApplication.java
@@ -0,0 +1,13 @@
+package hu.mundgus.udemxcarrent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemxcarrentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdemxcarrentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/mundgus/udemxcarrent/UdemxcarrentApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.mundgus.udemxcarrent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdemxcarrentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
