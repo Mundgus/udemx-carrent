76ffe24f0091b425c2c9b2e88b7341d959b65140
mundgus <fabnor99@gmail.com>
2025-03-04 12:27:01 +0100

    feat: get all cars api

--- src/main/java/hu/mundgus/udemxcarrent/controller/CarController.java
+++ src/main/java/hu/mundgus/udemxcarrent/controller/CarController.java
@@ -1,8 +1,14 @@
 package hu.mundgus.udemxcarrent.controller;
 
+import hu.mundgus.udemxcarrent.dto.CarDto;
+import hu.mundgus.udemxcarrent.model.Car;
 import hu.mundgus.udemxcarrent.repository.CarRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class CarController {
@@ -12,5 +18,27 @@ public class CarController {
     @Autowired
     public CarController(CarRepository carRepository) {
         this.carRepository = carRepository;
+    }
+
+    @GetMapping("/api/cars")
+    public List<CarDto> getAllCars() {
+        return carRepository.findAll()
+                .stream()
+                .map(this::toCarDto)
+                .collect(Collectors.toList());
+    }
+
+    private CarDto toCarDto(Car car) {
+        CarDto carDto = new CarDto();
+
+        carDto.setId(car.getId());
+        carDto.setPlateNumber(car.getPlateNumber());
+        carDto.setDailyFee(car.getDailyFee());
+        carDto.setFuelType(car.getFuelType());
+        carDto.setFuelConsumption(car.getFuelConsumption());
+        carDto.setActive(car.getActive());
+        carDto.setDescription(car.getDescription());
+
+        return carDto;
     }
 }
