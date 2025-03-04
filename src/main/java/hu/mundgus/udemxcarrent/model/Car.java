24379207f2019e9f271fef8f0124caf4026eb72b
mundgus <fabnor99@gmail.com>
2025-03-04 12:44:24 +0100

    refactor: move Car model accessors

--- src/main/java/hu/mundgus/udemxcarrent/model/Car.java
+++ src/main/java/hu/mundgus/udemxcarrent/model/Car.java
@@ -13,6 +13,63 @@ import java.util.List;
 @Entity
 @Table(name = "cars")
 public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(
+            nullable = false,
+            unique = true,
+            length = 32
+    )
+    private String plateNumber;
+
+    @Column(
+            nullable = false
+    )
+    private Integer dailyFee;
+
+    @Column(
+            nullable = false
+    )
+    private String fuelType;
+
+    @Column(
+            nullable = false
+    )
+    private Integer fuelConsumption;
+
+    @Column(
+            nullable = false
+    )
+    private Boolean isActive;
+
+    private String description;
+
+    @OneToMany(
+            mappedBy = "car"
+    )
+    private List<CarImage> carImages;
+
+    @OneToMany(
+            mappedBy = "car"
+    )
+    private List<Booking> bookings;
+
+
+    public Car() {}
+
+    public Car(Integer id, String plateNumber, Integer dailyFee, String fuelType, Integer fuelConsumption, Boolean isActive, String description) {
+        this.id = id;
+        this.plateNumber = plateNumber;
+        this.dailyFee = dailyFee;
+        this.fuelType = fuelType;
+        this.fuelConsumption = fuelConsumption;
+        this.isActive = isActive;
+        this.description = description;
+    }
+
+
     public Integer getId() {
         return id;
     }
@@ -83,61 +140,5 @@ public class Car {
 
     public void setBookings(List<Booking> bookings) {
         this.bookings = bookings;
-    }
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
-
-    @Column(
-            nullable = false,
-            unique = true,
-            length = 32
-    )
-    private String plateNumber;
-
-    @Column(
-            nullable = false
-    )
-    private Integer dailyFee;
-
-    @Column(
-            nullable = false
-    )
-    private String fuelType;
-
-    @Column(
-            nullable = false
-    )
-    private Integer fuelConsumption;
-
-    @Column(
-            nullable = false
-    )
-    private Boolean isActive;
-
-    private String description;
-
-    @OneToMany(
-            mappedBy = "car"
-    )
-    private List<CarImage> carImages;
-
-    @OneToMany(
-            mappedBy = "car"
-    )
-    private List<Booking> bookings;
-
-
-    public Car() {}
-
-    public Car(Integer id, String plateNumber, Integer dailyFee, String fuelType, Integer fuelConsumption, Boolean isActive, String description) {
-        this.id = id;
-        this.plateNumber = plateNumber;
-        this.dailyFee = dailyFee;
-        this.fuelType = fuelType;
-        this.fuelConsumption = fuelConsumption;
-        this.isActive = isActive;
-        this.description = description;
     }
 }
