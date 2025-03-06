aefb75251e972efec839dffc089c10c82589e3a8
mundgus <fabnor99@gmail.com>
2025-03-06 23:40:53 +0100

    feet: get available cars

--- src/main/java/hu/mundgus/udemxcarrent/controller/CarController.java
+++ src/main/java/hu/mundgus/udemxcarrent/controller/CarController.java
@@ -8,11 +8,9 @@ import hu.mundgus.udemxcarrent.repository.CarImageRepository;
 import hu.mundgus.udemxcarrent.repository.CarRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -30,6 +28,17 @@ public class CarController {
     @GetMapping("/api/cars")
     public List<CarDto> getAllCars() {
         return carRepository.findAll()
+                .stream()
+                .map(this::toCarDto)
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("/api/cars/{startDate}/{endDate}")
+    public List<CarDto> getAvailableCars(
+            @PathVariable LocalDate startDate,
+            @PathVariable LocalDate endDate
+    ) {
+        return carRepository.findAvailableCars(startDate, endDate)
                 .stream()
                 .map(this::toCarDto)
                 .collect(Collectors.toList());
--- src/main/java/hu/mundgus/udemxcarrent/repository/CarRepository.java
+++ src/main/java/hu/mundgus/udemxcarrent/repository/CarRepository.java
@@ -2,9 +2,19 @@ package hu.mundgus.udemxcarrent.repository;
 
 import hu.mundgus.udemxcarrent.model.Car;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 public interface CarRepository extends JpaRepository<Car, Long> {
     Optional<Car> findByPlateNumber(String plateNumber);
+
+    @Query("SELECT c FROM Car c WHERE c.isActive = true AND c.id NOT IN (" +
+            "SELECT b.car.id FROM Booking b " +
+            "WHERE (b.startDate BETWEEN :startDate AND :endDate) " +
+            "OR (b.endDate BETWEEN :startDate AND :endDate))")
+    List<Car> findAvailableCars(@Param("startDate") LocalDate startDate, @Param("endDate") LocalDate endDate);
 }
